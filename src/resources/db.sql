13ede138b9cffc41c40c9c7bee79f1f9ad953a29
roshanmutha <rcmutha@gmail.com>
2015-10-23 14:18:40 +0530

    Updated SQL File for mysql db.

--- src/resources/db.sql
+++ src/resources/db.sql
@@ -2,7 +2,7 @@ CREATE TABLE `department` (
   `department_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `dept_name` varchar(50) NOT NULL DEFAULT '0',
   PRIMARY KEY (`department_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=115 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=116 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `employee` (
   `employee_id` bigint(10) NOT NULL AUTO_INCREMENT,
@@ -14,4 +14,24 @@ CREATE TABLE `employee` (
   PRIMARY KEY (`employee_id`),
   KEY `FK_DEPT` (`department_id`),
   CONSTRAINT `FK_DEPT` FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `item` (
+  `item_id` bigint(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(45) NOT NULL,
+  `created_date` varchar(45) NOT NULL,
+  `amount` bigint(10) NOT NULL,
+  `transaction_id` bigint(10) NOT NULL,
+  PRIMARY KEY (`item_id`),
+  KEY `FKTransactionID_idx` (`transaction_id`),
+  CONSTRAINT `FKTransactionID` FOREIGN KEY (`transaction_id`) REFERENCES `transaction` (`transaction_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `transaction` (
+  `transaction_id` bigint(10) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `created_date` datetime NOT NULL,
+  PRIMARY KEY (`transaction_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
